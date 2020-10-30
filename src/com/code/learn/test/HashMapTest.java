399a152222944272d16741cfc7cbd2c18767341f
stealhao <penghao@zhiyuanhui.com>
2020-10-30 15:39:05 +0800

    初始化环境

---
+++ src/com/code/learn/test/HashMapTest.java
@@ -0,0 +1,23 @@
+package com.code.learn.test;
+
+import java.util.HashMap;
+
+/**
+ * @ClassName: HashMapTest
+ * @Description:
+ * @Author: jackson
+ * @Date: 2020/10/30 下午2:42
+ * @Version: v1.0
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String,Double> hashMap = new HashMap<>();
+        hashMap.put("key1",0.1);
+        hashMap.put("key2",0.2);
+        hashMap.put("key3",0.3);
+        hashMap.put("key4",0.4);
+        hashMap.put("key5",0.5);
+    }
+
+}
